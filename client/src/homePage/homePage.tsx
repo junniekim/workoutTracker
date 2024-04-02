e0e0c96d65198dbaac466b8484c2d91f7cd1cd53
Sijun Kim <junniekim12@gmail.com>
2024-04-02 01:18:30 -0400

    Homepage work starts

--- client/src/homePage/homePage.tsx
+++ client/src/homePage/homePage.tsx
@@ -1,12 +1,16 @@
+import creator from "../assets/creator.png";
 const HomePage = () => {
   return (
-    <div className="row">
-      <div className="col-8">
+    <div className="row text-right">
+      <div className="col-7">
         <h2>New Generation Workout Tracker</h2>
         <p>This workout tracker is so awesome this workout tracker does</p>
       </div>
-      <div className="col-4">Picture goes here</div>
+      <div className="col-5">
+        <img src={creator} alt="Creator" width="400px" />
+      </div>
     </div>
   );
 };
+
 export default HomePage;
