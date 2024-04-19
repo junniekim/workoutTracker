007343b6ab867ad8a243f68ace89e773f07396a7
Sijun Kim <junniekim12@gmail.com>
2024-04-18 22:34:00 -0400

    Filter bar Working

--- client/src/workoutListPage/workoutListPage.tsx
+++ client/src/workoutListPage/workoutListPage.tsx
@@ -3,14 +3,21 @@ import Dropdown from "react-dropdown";
 import noImage from "../assets/noImage.png";
 import "react-dropdown/style.css";
 import Workout from "./workout";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useUser } from "../SesssionManager/session";
 import Select from "react-select";
 const WorkoutListPage = () => {
+  //clear button
+  //STATES
+  //current user data
   const { userData } = useUser();
+  //valid set of options for setting target
   const [validTarget, setValidTarget] = useState<any[]>([]);
-  const [currentTarget, setCurrentTarget] = useState([]);
-
+  //valid set of paths for each target
+  const [validTargetPath, setValidTargetPath] = useState<any>([]);
+  //current path selected
+  const [currentTarget, setCurrentTarget] = useState<any[][]>([]);
+  //workout List available
   const [workoutList, setWorkoutList] = useState<
     Array<{
       dateentered: string;
@@ -20,6 +27,7 @@ const WorkoutListPage = () => {
       target: string[];
     }>
   >([]);
+  //custom workout list available
   const [customWorkoutList, setCustomWorkoutList] = useState<
     Array<{
       dateentered: string;
@@ -29,6 +37,8 @@ const WorkoutListPage = () => {
       target: string[];
     }>
   >([]);
+
+  //fetch all workout data
   const query = `http://localhost:3000/workout`;
   if (workoutList.length === 0) {
     fetch(query)
@@ -36,6 +46,7 @@ const WorkoutListPage = () => {
         return response.json();
       })
       .then((data) => {
+        //Filter array so that each element has a unique path to the target
         let distinctWorkouts = data.filter(
           (workout: any, index: number, array: any[]) => {
             return !array
@@ -46,6 +57,14 @@ const WorkoutListPage = () => {
               );
           }
         );
+        //Set valid and unique path
+        let distinctWorkoutPath = distinctWorkouts.map(
+          (workout: any, index: number) => {
+            return workout.target;
+          }
+        );
+        setValidTargetPath(distinctWorkoutPath);
+        //Set unique and valid target options
         let distinctWorkoutTargets = distinctWorkouts.map((workout: any) => {
           return {
             value: workout.target[workout.target.length - 1],
@@ -53,6 +72,7 @@ const WorkoutListPage = () => {
           };
         });
         setValidTarget(distinctWorkoutTargets);
+        //Filter out custom workouts
         let filteredWorkouts = data.filter(
           (workout: any) => workout.custom === false
         );
@@ -62,6 +82,7 @@ const WorkoutListPage = () => {
         console.error(error);
       });
   }
+  //get custom workout list
   const customQuery = `http://localhost:3000/customWorkout/${userData?._id}`;
   if (customWorkoutList.length === 0) {
     fetch(customQuery)
@@ -76,12 +97,70 @@ const WorkoutListPage = () => {
       });
   }
 
-  const options = ["Upperbody", "Chest", "Upper Chest", "Cardio"];
+  let option1: any[] = [];
+  let option2: any[] = [];
+  let option3: any[] = [];
+  let option4: any[] = [];
+
+  validTargetPath.forEach((path: any, index: number) => {
+    if (path[0] && !option1.some((option: any) => option.value === path[0])) {
+      option1.push({ value: path[0], label: path[0] });
+    }
+    if (path[1] && !option2.some((option: any) => option.value === path[1])) {
+      option2.push({ value: path[1], label: path[1], previous: path[0] });
+    }
+    if (path[2] && !option3.some((option: any) => option.value === path[2])) {
+      option3.push({ value: path[2], label: path[2], previous: path[1] });
+    }
+    if (path[3] && !option4.some((option: any) => option.value === path[3])) {
+      option4.push({ value: path[3], label: path[3], previous: path[2] });
+    }
+  });
+
+  //States that controls view mode
   const [adding, setAdding] = useState(false);
   const [customOnly, setCustomOnly] = useState(false);
   const [editing, setEditing] = useState(false);
+
+  // 4 states for path selection
+  const [path1, setPath1] = useState<any>("");
+  const [path2, setPath2] = useState<any>("");
+  const [path3, setPath3] = useState<any>("");
+  const [path4, setPath4] = useState<any>("");
+
+  useEffect(() => {
+    setPath2("");
+    setPath3("");
+    setPath4("");
+  }, [path1]);
+  useEffect(() => {
+    setPath3("");
+    setPath4("");
+  }, [path2]);
+  useEffect(() => {
+    setPath4("");
+  }, [path3]);
+  const clearPath = () => {
+    setPath1("");
+    setPath2("");
+    setPath3("");
+    setPath4("");
+  };
+  const test = () => {
+    console.log(path1);
+    console.log(path2);
+    console.log(path3);
+    console.log(path4);
+  };
   return (
     <div>
+      <button
+        onClick={() => {
+          test();
+        }}
+      >
+        testr
+      </button>
       <TitleHeader title="✏️My List"></TitleHeader>
       <div
         className="row d-flex flex-row"
@@ -94,19 +173,73 @@ const WorkoutListPage = () => {
           padding: "20px",
         }}
       >
-        <Dropdown
+        <Select
           className="mt-2 mb-2  col-12 col-md-3 col-lg-2" // need to be bigger on bigger screen
-          options={options}
-          value={options[3]}
+          options={option1}
           placeholder="Select an option"
-        ></Dropdown>
-        <h2 className="mt-2 mb-2 col-auto">»</h2>
-        <Dropdown
-          className=" mt-2 mb-2 col-12 col-md-3 col-lg-2" // need to be bigger on bigger screen
-          options={options}
-          value={options[0]}
-          placeholder="Select an option"
-        ></Dropdown>
+          value={path1}
+          onChange={(selectedOption) => setPath1(selectedOption)}
+        ></Select>
+
+        {path1 &&
+          option2.filter((option) => option.previous === path1?.value).length >
+            0 && (
+            <>
+              <h2 className="mt-2 mb-2 col-auto">»</h2>
+              <Select
+                className=" mt-2 mb-2 col-12 col-md-3 col-lg-2" // need to be bigger on bigger screen
+                options={option2.filter(
+                  (option) => option.previous === path1?.value
+                )}
+                placeholder="Select an option"
+                value={path2}
+                onChange={(selectedOption) => setPath2(selectedOption)}
+              ></Select>
+            </>
+          )}
+
+        {path2 &&
+          option3.filter((option) => option.previous === path2?.value).length >
+            0 && (
+            <>
+              <h2 className="mt-2 mb-2 col-auto">»</h2>
+              <Select
+                className=" mt-2 mb-2 col-12 col-md-3 col-lg-2" // need to be bigger on bigger screen
+                options={option3.filter(
+                  (option) => option.previous === path2?.value
+                )}
+                placeholder="Select an option"
+                value={path3}
+                onChange={(selectedOption) => setPath3(selectedOption)}
+              ></Select>
+            </>
+          )}
+
+        {path3 &&
+          option4.filter((option) => option.previous === path3?.value).length >
+            0 && (
+            <>
+              <h2 className="mt-2 mb-2 col-auto">»</h2>
+              <Select
+                className=" mt-2 mb-2 col-12 col-md-3 col-lg-2" // need to be bigger on bigger screen
+                options={option4.filter(
+                  (option) => option.previous === path3?.value
+                )}
+                placeholder="Select an option"
+                value={path4}
+                onChange={(selectedOption) => setPath4(selectedOption)}
+              ></Select>
+            </>
+          )}
+
+        <button
+          className=" mt-2 mb-2 col-12 col-md-3 col-lg-2 btn btn-danger" // need to be bigger on bigger screen
+          style={{ marginLeft: "20px" }}
+          disabled={path1 === ""}
+          onClick={() => clearPath()}
+        >
+          Clear
+        </button>
 
         {adding ? (
           <div
@@ -214,9 +347,36 @@ const WorkoutListPage = () => {
       <div className="mt-4 row d-flex flex-row">
         {!customOnly &&
           !editing &&
-          workoutList.map((workout: any, index: number) => (
+          workoutList
+            .filter((workout: any) => {
+              if (path4) return workout.target.includes(path4.value);
+              if (path3) return workout.target.includes(path3.value);
+              if (path2) return workout.target.includes(path2.value);
+              if (path1) return workout.target.includes(path1.value);
+              return true; // if none of the paths are defined, don't filter out any workouts
+            })
+            .map((workout: any, index: number) => (
+              <Workout
+                key={index}
+                description={workout.target[workout.target.length - 1]}
+                custom={workout.custom}
+                editing={editing}
+                image={workout.workout_picture}
+                title={workout.workout_name}
+              ></Workout>
+            ))}
+        {customWorkoutList
+          .filter((workout: any) => {
+            if (path4) return workout.target.includes(path4.value);
+            if (path3) return workout.target.includes(path3.value);
+            if (path2) return workout.target.includes(path2.value);
+            if (path1) return workout.target.includes(path1.value);
+            return true; // if none of the paths are defined, don't filter out any workouts
+          })
+          .map((workout: any, index: number) => (
             <Workout
               key={index}
+              validTarget={validTarget}
               description={workout.target[workout.target.length - 1]}
               custom={workout.custom}
               editing={editing}
@@ -224,17 +384,6 @@ const WorkoutListPage = () => {
               title={workout.workout_name}
             ></Workout>
           ))}
-        {customWorkoutList.map((workout: any, index: number) => (
-          <Workout
-            key={index}
-            validTarget={validTarget}
-            description={workout.target[workout.target.length - 1]}
-            custom={workout.custom}
-            editing={editing}
-            image={workout.workout_picture}
-            title={workout.workout_name}
-          ></Workout>
-        ))}
       </div>
     </div>
   );
